273cdde6a4bb74e419c4bed38e664e0911417d2a
Michael Antis <antis.michael@gmail.com>
2021-08-25 15:30:59 -0400

    add listings actions only: [:index, :new, :create, :show] and their views

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,30 @@
 class ListingsController < ApplicationController
 
   def index
+    @listings = Listing.all
   end
 
   def new
-    
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save 
+      flash[:notice] = "Listing successfully created"
+      redirect_to listing_path @listing
+    else
+      flash[:alert] = "Error: #{ @listing.errors.full_messages }"
+      render :new
+    end
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end  
+
+  private
+  def listing_params
+    params.require(:listing).permit(:address, :unit, :bedrooms, :bathrooms, :sqft)
   end
 end
